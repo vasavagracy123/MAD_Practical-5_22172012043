c9be052e9d6793567048d13221863a3093c8aa51
surekhabhanvar123 <surekhabenbhanvar12@gmail.com>
2023-08-25 14:17:06 +0530

    hy

--- app/src/main/java/com/example/mad_practical_5_22172012043/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_5_22172012043/MainActivity.kt
@@ -4,32 +4,46 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
+import android.provider.CallLog
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
+import androidx.core.text.parseAsHtml
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val browseButton:Button=findViewById(R.id.button_browse)
-        val editTextBrowser:EditText=findViewById(R.id.editTextText2)
+        val browseButton: Button =findViewById(R.id.button_Browse)
+        val editTextBrowser: EditText =findViewById(R.id.editTextText)
         browseButton.setOnClickListener {
-            openBrowser(editTextBrowser.text.toString())
+            openbrowser(editTextBrowser.text.toString())
         }
     }
-    fun openBrowser(url: String){
-       Intent(Intent.ACTION_VIEW, Uri.parse(url)).also { startActivity(it) }
+    fun openbrowser(url:String){
+        Intent(Intent.ACTION_VIEW,Uri.parse(url)).also{startActivity(it)
+
+        }
+        fun opencall(no:String){
+            Intent(Intent.ACTION_DIAL).setData(Uri.parse("tel:$no")).also{startActivity(it)}
+        }
     }
-    fun openCall(no:String){
-        Intent(Intent.ACTION_DIAL).setData(Uri.parse("tel:$no")).also {startActivity(it)}
-    }
-    fun openCallLog(){
+    fun openCalllog(){
+        Intent(Intent.ACTION_VIEW).setType(CallLog.Calls.CONTENT_TYPE).also { startActivity(it) }
 
     }
-    fun openGallery(){
+    fun openGallary(){
+        Intent(Intent.ACTION_VIEW).setType("image/*").also { startActivity(it) }
 
     }
-    fun openAlam(){
+    fun camera(){
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { startActivity(it) }
 
     }
+    fun Alaram(){
+        Intent(AlarmClock.ACTION_SHOW_ALARMS).also { startActivity(it) }
+
+    }
+
 }
